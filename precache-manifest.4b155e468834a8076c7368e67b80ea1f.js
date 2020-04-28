aba2fcd332b3eb2b51ff3fdddb5fbb8827c0ab3d
Santos Castaneda <santoscastaneda@Gs-iMac.local>
2020-04-28 00:17:29 -0700

    Updates

---
+++ precache-manifest.4b155e468834a8076c7368e67b80ea1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e8f535a28a28509c210eb82c50f931",
+    "url": "/store-app/index.html"
+  },
+  {
+    "revision": "b3ab74df28e638b22c26",
+    "url": "/store-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f717afff550234c41ea2",
+    "url": "/store-app/static/js/2.ac32e936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-app/static/js/2.ac32e936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ab74df28e638b22c26",
+    "url": "/store-app/static/js/main.891437a2.chunk.js"
+  },
+  {
+    "revision": "9e47326c658f808dad17",
+    "url": "/store-app/static/js/runtime-main.0a162cc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
